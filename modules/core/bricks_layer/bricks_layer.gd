19f53767077ef15e6042abae295a7e11015ffbd4
Igorrreha <igorecha.9999@gmail.com>
2023-09-16 17:09:14 +0700

    Improve BricksLayer
    - add broken and landed states
    - change inherited type
    - add signals channel

--- modules/core/bricks_layer/bricks_layer.gd
+++ modules/core/bricks_layer/bricks_layer.gd
@@ -1,29 +1,52 @@
-extends Area2D
+class_name BricksLayer
+extends CharacterBody2D
 
 
 enum State {
 	GRABBED,
-	FLYING_DOWN,
+	FALLING_DOWN,
+	LANDED,
+	BROKEN,
 }
 
 
 @export var _fly_down_speed := 675.0
 @export var _arm_signals_channel: ArmSignalsChannel
+@export var _bricks_layers_signals_channel: BricksLayersSignalsChannel
 
 var _cur_state: State = State.GRABBED
+
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var _valid_collision_detector: RayCast2D = $ValidCollisionDetector
 
 
 func _ready() -> void:
 	_arm_signals_channel.grabber_relaxed.connect(_on_grabber_relaxed)
 
 
-func _process(delta: float) -> void:
-	if _cur_state == State.FLYING_DOWN:
-		position.y += _fly_down_speed * delta
+func _physics_process(delta: float) -> void:
+	if _cur_state != State.FALLING_DOWN:
+		return
+	
+	if is_on_floor():
+		if _valid_collision_detector.is_colliding():
+			_cur_state = State.LANDED
+			_bricks_layers_signals_channel.layer_landed.emit(self)
+		else:
+			_broke()
+	
+	velocity.y = _fly_down_speed
+	move_and_slide()
+
+
+func _broke() -> void:
+	_cur_state = State.BROKEN
+	_bricks_layers_signals_channel.layer_broken.emit(self)
+	queue_free()
 
 
 func _on_grabber_relaxed() -> void:
 	if _cur_state == State.GRABBED:
-		_cur_state = State.FLYING_DOWN
+		_cur_state = State.FALLING_DOWN
 	
 	_arm_signals_channel.grabber_relaxed.disconnect(_on_grabber_relaxed)
---
+++ modules/core/bricks_layer/bricks_layers_signals_channel.gd
@@ -0,0 +1,6 @@
+class_name BricksLayersSignalsChannel
+extends SignalsChannel
+
+
+signal layer_landed(layer: BricksLayer)
+signal layer_broken(layer: BricksLayer)
