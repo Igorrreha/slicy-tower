240b28da83fcc70e6df462bcca79d6ba00e0859d
Igorrreha <igorecha.9999@gmail.com>
2023-09-21 21:37:43 +0700

    Fix building twitching on new layer landing

--- modules/visuals/building_instability_visualizer.gd
+++ modules/visuals/building_instability_visualizer.gd
@@ -3,31 +3,28 @@ extends AnimationPlayer
 
 @export var _building: Building
 @export var _building_instability_storage: BuildingInstabilityStorage
-@export var _loop_offset_curve: Curve
-@export var _delta_offset := 200
+@export var _max_offset_delta := 200
 
 @export var _loop_position := 0.5
+@export var _loop_duration := 2.2
 
 @onready var _initial_position := _building.position
 
 
 func _ready() -> void:
 	_building_instability_storage.bind("instability", _on_building_instability_updated)
+	play("idle", 0.5, 1 / _loop_duration)
 
 
 func _on_building_instability_updated() -> void:
-#	var max_offset = (_loop_offset_curve.sample(1.0)
-#		* _delta_offset * _building_instability_storage.instability) * 2
-#	var new_loop_position = (0 if _building.position == _initial_position
-#		else max_offset / (_building.position.x - _initial_position.x) + 0.5)
-#
-#	print(new_loop_position)
-#	stop(0)
-#	play("idle", new_loop_position * get_animation("idle").length)
-	pass
+	var max_offset = _max_offset_delta * _building_instability_storage.instability
+	var new_loop_position = (0.5 if _building.position == _initial_position
+		else (_building.position.x - _initial_position.x) / max_offset / 2.0 + 0.5)
+	
+	seek(new_loop_position)
 
 
 func _process(delta: float) -> void:
-	var offset = (_loop_offset_curve.sample(_loop_position)
-		* _delta_offset * _building_instability_storage.instability)
+	var offset = (_max_offset_delta * _building_instability_storage.instability
+		* _loop_position)
 	_building.position = _initial_position + Vector2(offset, 0)
