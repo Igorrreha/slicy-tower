e38a0a70159546bb44016cdf06a299821e9c431f
Igorrreha <igorecha.9999@gmail.com>
2023-09-22 21:31:19 +0700

    Make building movement smooth

--- modules/visuals/building_instability_visualizer.gd
+++ modules/visuals/building_instability_visualizer.gd
@@ -5,6 +5,7 @@ extends AnimationPlayer
 @export var _building_instability_storage: BuildingInstabilityStorage
 @export var _max_offset_delta := 200
 
+@export var _loop_offset_curve: Curve
 @export var _loop_position := 0.5
 @export var _loop_duration := 2.2
 
@@ -17,14 +18,29 @@ func _ready() -> void:
 
 
 func _on_building_instability_updated() -> void:
+	var offset = _building.position.x - _initial_position.x
 	var max_offset = _max_offset_delta * _building_instability_storage.instability
-	var new_loop_position = (0.5 if _building.position == _initial_position
-		else (_building.position.x - _initial_position.x) / max_offset / 2.0 + 0.5)
 	
+	var new_loop_position = _get_curve_x_by_y(_loop_offset_curve, offset / max_offset)
 	seek(new_loop_position)
+
+
+func _get_curve_x_by_y(curve: Curve, y: float) -> float:
+	var offset_position = 0.5
+	var iteration_part_size = 0.5
+	for i in range(100):
+		var offset_value = curve.sample(offset_position)
+		
+		if y == offset_value:
+			return offset_position
+		
+		iteration_part_size /= 2
+		offset_position += iteration_part_size * (1 if offset_value < y else -1)
+	
+	return offset_position
 
 
 func _process(delta: float) -> void:
 	var offset = (_max_offset_delta * _building_instability_storage.instability
-		* _loop_position)
+		* _loop_offset_curve.sample(_loop_position))
 	_building.position = _initial_position + Vector2(offset, 0)
